eddecb019a46f318dfc1438e8ac364c4b23b3761
MiniCongee <replybber@gmail.com>
2020-08-23 20:09:05 +0800

    warning use other HTML element in g-button-group & fixed margin CSS Style in g-button-group

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,7 +4,18 @@
   </div>
 </template>
 
-<script></script>
+<script>
+  export default {
+    mounted() {
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase()
+        if (name !== 'button') {
+          console.warn(`g-button-group 的子元素应该为 g-button，您使用了${name}`)
+        }
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
   .g-button-group {
@@ -12,7 +23,9 @@
     vertical-align: middle;
     > .g-button {
       border-radius: 0;
-      margin-left: -1px;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
       &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
