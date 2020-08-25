82a5afae89eadf6113d1c2204416b3aa9c69d4bc
MiniCongee <replybber@gmail.com>
2020-08-25 20:58:13 +0800

    create Input Component automatic testing with input.test.js

---
+++ test/input.test.js
@@ -0,0 +1,82 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+  it('存在', () => {
+    expect(Input).to.exist
+  })
+
+  describe('props', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
+
+    it('接受 value', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'inputString'
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('inputString')
+    })
+
+    it('接收 disabled', () => {
+      vm = new Constructor({
+        propsData: {
+          disabled: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(true)
+    })
+
+    it('接收 readonly', () => {
+      vm = new Constructor({
+        propsData: {
+          readonly: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      // readOnly必须采用小驼峰
+      expect(inputElement.readOnly).to.equal(true)
+    })
+    it('接收 error', () => {
+      vm = new Constructor({
+        propsData: {
+          error: 'errorMessageString'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+      const errorMessage = vm.$el.querySelector('.errorMessage')
+      expect(errorMessage.innerText).to.equal('errorMessageString')
+    })
+  })
+  describe('事件', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
+    it('支持 change/input/focus/blur 事件', () =>{
+      ['change', 'input', 'focus', 'blur']
+        .forEach((eventName) => {
+          vm = new Constructor({}).$mount()
+          const callback = sinon.fake()
+          vm.$on(eventName, callback)
+          // 触发input内的事件
+          let event = new Event(eventName)
+          let inputElement = vm.$el.querySelector('input')
+          inputElement.dispatchEvent(event)
+          expect(callback).to.have.been.calledWith(event)
+        })
+    })
+  })
+})
