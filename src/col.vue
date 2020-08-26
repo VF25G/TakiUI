4588cbfebd1d14782a68020ab98ae8fa049099b4
MiniCongee <replybber@gmail.com>
2020-08-26 16:39:24 +0800

    rebuild code in col & row

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
-    <div style="border: 1px solid blue; height: 100px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass"
+       :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -21,6 +19,21 @@
     data() {
       return {
         gutter: 0
+      }
+    },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return {
+          [`col-${span}`]: span,
+          [`offset-${offset}`]: offset
+        }
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px'
+        }
       }
     }
   }
