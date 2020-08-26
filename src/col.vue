6478e1be31862d7db38bba43844d366cfd213ad0
MiniCongee <replybber@gmail.com>
2020-08-26 22:43:21 +0800

    completed basic Responsive-CSS-Style

--- src/col.vue
+++ src/col.vue
@@ -6,6 +6,16 @@
 </template>
 
 <script>
+  let validator = (value) => {
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
+      }
+    })
+    return valid
+  }
   export default {
     name: 'webUICol',
     props: {
@@ -14,6 +24,26 @@
       },
       offset: {
         type: [Number, String]
+      },
+      phone: {
+        type: Object,
+        validator
+      },
+      ipad: {
+        type: Object,
+        validator
+      },
+      narrowPc: {
+        type: Object,
+        validator
+      },
+      pc: {
+        type: Object,
+        validator
+      },
+      widePc: {
+        type: Object,
+        validator
       }
     },
     data() {
@@ -23,10 +53,15 @@
     },
     computed: {
       colClass() {
-        let {span, offset} = this
+        let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
         return {
           [`col-${span}`]: span,
-          [`offset-${offset}`]: offset
+          [`offset-${offset}`]: offset,
+          [`col-phone-${phone.span}`]: phone,
+          [`col-ipad-${ipad.span}`]: ipad,
+          [`col-narrow-pc-${narrowPc.span}`]: narrowPc,
+          [`col-pc-${pc.span}`]: pc,
+          [`col-wide-pc-${widePc.span}`]: widePc,
         }
       },
       colStyle() {
@@ -52,6 +87,76 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+    @media (max-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 577px) and (max-width: 768px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 769px) and (max-width: 992px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 993px) and (max-width: 1200px) {
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
