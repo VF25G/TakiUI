c2547104bee1374098b8827825d5cec5df97ad9e
MiniCongee <replybber@gmail.com>
2020-08-28 00:05:21 +0800

    completed Basic for Layout

---
+++ src/sider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'webUISider'
+  }
+</script>
+
+<style lang="scss">
+  .sider {
+  }
+</style>
