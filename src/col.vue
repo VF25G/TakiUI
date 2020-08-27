5b58dfb2d30d94209dc7ad1ab858f5b1f3355e86
MiniCongee <replybber@gmail.com>
2020-08-27 16:30:41 +0800

    update Responsive-CSS-Style

--- src/col.vue
+++ src/col.vue
@@ -50,14 +50,14 @@
     computed: {
       colClass() {
         let {span, offset, ipad, narrowPc, pc, widePc} = this
-        return {
-          [`col-${span}`]: span,
-          [`offset-${offset}`]: offset,
-          [`col-ipad-${ipad.span}`]: ipad,
-          [`col-narrow-pc-${narrowPc.span}`]: narrowPc,
-          [`col-pc-${pc.span}`]: pc,
-          [`col-wide-pc-${widePc.span}`]: widePc,
-        }
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`,
+          ... (ipad ? [`col-ipad${ipad.span}`] : []),
+          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+          ... (pc ? [`col-pc${pc.span}`] : []),
+          ... (widePc ? [`col-wide-pc${widePc.span}`] : []),
+        ]
       },
       colStyle() {
         return {
@@ -84,7 +84,7 @@
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 577px) and (max-width: 768px) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -98,7 +98,7 @@
         }
       }
     }
-    @media (min-width: 769px) and (max-width: 992px) {
+    @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -112,7 +112,7 @@
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
