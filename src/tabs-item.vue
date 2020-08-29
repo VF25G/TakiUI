45c66cb7d311316b32be9b756f895cad68fcd548
MiniCongee <replybber@gmail.com>
2020-08-29 22:04:58 +0800

    support disabled tabs-item

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="logName" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -26,12 +26,14 @@
     computed: {
       classes() {
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled
         }
       }
     },
     methods: {
-      logName() {
+      onClick() {
+        if (this.disabled) { return }
         this.eventBus.$emit('update:selected', this.name, this)
       }
     },
@@ -45,6 +47,7 @@
 
 <style lang="scss" scoped>
   $blue: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink: 0;
     padding: 0 1em;
@@ -55,6 +58,10 @@
     &.active {
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor: not-allowed;
     }
   }
 </style>
