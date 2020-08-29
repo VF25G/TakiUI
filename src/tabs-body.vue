96a90d74096ebe8201056fa1bfe243e288811224
MiniCongee <replybber@gmail.com>
2020-08-29 15:41:42 +0800

    create Tabs & Tabs children Component with basic props

---
+++ src/tabs-body.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs-body">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'webUITabsBody'
+  }
+</script>
+
+<style>
+  .tabs-body {
+
+  }
+</style>
