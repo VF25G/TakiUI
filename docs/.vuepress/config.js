4bcc5b4e85d8fd4aeb524fc553d3e892630fdc3e
MiniCongee <replybber@gmail.com>
2020-09-05 17:54:02 +0800

    update Home Page & append Logo, favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,26 @@
 module.exports = {
-  base: '/webUI/',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  base: '/TakiUI/',
+  title: 'Taki UI',
+  description: '基于Vue2.0的轻型UI框架',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     nav: [
-      {text: '主页', link: '/'},
-      {text: '交流', link: 'https://google.com'}
+      { text: '主页', link: '/' },
+      { text: 'Github', link: 'https://github.com/VF25G/' },
+      { text: '欢迎star', link: 'https://github.com/VF25G/TakiUI'  },
     ],
     sidebar: [
+      { title:'介绍',
+        collapsable: false,
+        children:[
+          '/introduce/',
+        ],
+      },
       {
         title: '入门',
+        collapsable: false,
         children: [
           '/install/',
           '/get_started/'
@@ -17,6 +28,7 @@ module.exports = {
       },
       {
         title: '组件',
+        collapsable: false,
         children: [
           '/components/button',
           '/components/tabs',
---
+++ docs/introduce/README.md
@@ -0,0 +1,16 @@
+# Taki UI
+[![Build Status](https://travis-ci.org/VF25G/TakiUI.svg?branch=master)](https://travis-ci.org/VF25G/TakiUI)
+![](https://img.shields.io/badge/language-JavaScript-yellow.svg)
+![](https://img.shields.io/badge/license-MIT-000000.svg)
+
+Taki UI 作为轻量型的 UI 框架，提供了一些常用组件，适合于桌面端使用。
+
+组件：Button按钮、Input输入框、Grid网格、Layout布局、Toast吐司、Tabs标签页、Popover气泡卡片、Collapse折叠面板
+
+## 关于Taki UI
+
+在学习 Vue2 的过程中，参考了 ELement 和 Ant Design，基于个人理解、能力，所尝试搭建的一个简易 UI 框架。
+由于能力有效，Taki UI 仅适合于个人学习使用，请勿在生产环境中使用本框架。
+
+## 联系方式
+邮箱：replybber@gmail.com
