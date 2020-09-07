56161352c82b8a1a303b1d48e5cbb1a3119b5429
MiniCongee <replybber@gmail.com>
2020-09-07 22:41:51 +0800

    update attributes content for collapse-demos

--- docs/.vuepress/components/collapse-demos-3.vue
+++ docs/.vuepress/components/collapse-demos-3.vue
@@ -26,6 +26,70 @@
       <pre><code class="html">{{formatCode(collapseCode)}}</code></pre>
     </div>
 
+    <section>
+      <h2>Collapse Attributes</h2>
+      <div class="attr">
+        <table>
+          <thead>
+          <tr>
+            <th>参数</th>
+            <th class="des">说明</th>
+            <th class="type">类型</th>
+            <th>可选值</th>
+            <th>默认值</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>selected</td>
+            <td class="des">数组：为展开的collapseItem的name属性的集合。可配合.sync使用。</td>
+            <td class="type">Array</td>
+            <td>——</td>
+            <td>——</td>
+          </tr>
+          <tr>
+            <td>single</td>
+            <td class="des">设置是否允许展开多个collapseItem</td>
+            <td class="type">Boolean</td>
+            <td>——</td>
+            <td>false</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+    <section>
+      <h2>CollapseItem Attributes</h2>
+      <div class="attr">
+        <table>
+          <thead>
+          <tr>
+            <th>参数</th>
+            <th class="des">说明</th>
+            <th class="type">类型</th>
+            <th>可选值</th>
+            <th>默认值</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>title</td>
+            <td class="des">必要属性：collapse-item 的标题内容</td>
+            <td class="type">String</td>
+            <td>——</td>
+            <td>——</td>
+          </tr>
+          <tr>
+            <td>name</td>
+            <td class="des">必要属性，且需保证唯一性</td>
+            <td class="type">String</td>
+            <td>——</td>
+            <td>——</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -59,3 +123,14 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .attr {
+    td {
+      text-align:center;
+    }
+    .des {
+      text-align: left;
+    }
+  }
+</style>
